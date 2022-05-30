00b447f69d00c725c8633053e936bccbc22dc8c2
Joanna May <joannamadisonmay@gmail.com>
2022-05-29 21:33:25 -0500

    Update linter warnings

--- GoDotAddon/src/Main.cs
+++ GoDotAddon/src/Main.cs
@@ -1,29 +1,11 @@
 ﻿namespace Chickensoft.GoDotAddon {
   using System.Threading.Tasks;
   using CliFx;
-  using CliFx.Attributes;
-  using CliFx.Infrastructure;
 
-  [Command]
-  public class GoDotAddonApp : ICommand {
-    public ValueTask ExecuteAsync(IConsole console) {
-      console.Output.WriteLine("Thanks for choosing GoDotAddon!");
-      return default;
-    }
-  }
-
-  internal class GoDotAddon {
-    private static async Task<int> Main(string[] args) {
-      var app = new CliApplicationBuilder()
-          .AddCommandsFromThisAssembly()
-          .Build();
-
-      // Console.WriteLine($"Launched from {Environment.CurrentDirectory}");
-      // Console.WriteLine($"Physical location {AppDomain.CurrentDomain.BaseDirectory}");
-      // Console.WriteLine($"AppContext.BaseDir {AppContext.BaseDirectory}");
-      // Console.WriteLine($"Runtime Call {Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName)}")
-
-      return await app.RunAsync(args);
-    }
+  public class GoDotAddon {
+    public static ValueTask<int> Main(string[] args)
+      => new CliApplicationBuilder()
+        .AddCommandsFromThisAssembly()
+        .Build().RunAsync(args);
   }
 }
