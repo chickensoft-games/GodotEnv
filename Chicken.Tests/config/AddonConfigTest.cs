2814bf7552d7167c058f96a2d127ddc3898f0b51
Joanna May <joannamadisonmay@gmail.com>
2022-09-03 11:37:32 -0500

    Add optional symlink flag to addon config

--- Chicken.Tests/config/AddonConfigTest.cs
+++ Chicken.Tests/config/AddonConfigTest.cs
@@ -8,18 +8,21 @@ namespace Chickensoft.Chicken.Tests {
       = "https://github.com/chickensoft-games/Chicken";
     private const string SUBFOLDER = "Chicken";
     private const string CHECKOUT = "main";
+    private const bool SYMLINK = true;
 
     [Fact]
     public void Deserializes() {
       var json = "{" +
         $"  \"url\": \"{URL}\"," +
         $"  \"subfolder\": \"{SUBFOLDER}\"," +
-        $"  \"checkout\": \"{CHECKOUT}\"" +
+        $"  \"checkout\": \"{CHECKOUT}\"," +
+        $"  \"symlink\": \"{SYMLINK}\"" +
       "}";
       var config = JsonConvert.DeserializeObject<AddonConfig>(json);
       config.ShouldNotBeNull();
       config.Subfolder.ShouldBe(SUBFOLDER);
       config.Checkout.ShouldBe(CHECKOUT);
+      config.Symlink.ShouldBe(SYMLINK);
     }
 
     [Fact]
@@ -27,12 +30,14 @@ namespace Chickensoft.Chicken.Tests {
       var json = "{" +
         $"  \"url\": \"{URL}\"," +
         $"  \"subfolder\": null," +
-        $"  \"checkout\": null" +
+        $"  \"checkout\": null," +
+        $"  \"symlink\": true" +
       "}";
       var config = JsonConvert.DeserializeObject<AddonConfig>(json);
       config.ShouldNotBeNull();
       config.Subfolder.ShouldBe(IApp.DEFAULT_SUBFOLDER);
       config.Checkout.ShouldBe(IApp.DEFAULT_CHECKOUT);
+      config.Symlink.ShouldBe(true);
     }
 
     [Fact]
@@ -42,6 +47,7 @@ namespace Chickensoft.Chicken.Tests {
       config.ShouldNotBeNull();
       config.Subfolder.ShouldBe(IApp.DEFAULT_SUBFOLDER);
       config.Checkout.ShouldBe(IApp.DEFAULT_CHECKOUT);
+      config.Symlink.ShouldBe(false);
     }
   }
 }
--- Chicken/src/config/AddonConfig.cs
+++ Chicken/src/config/AddonConfig.cs
@@ -8,16 +8,20 @@ namespace Chickensoft.Chicken {
     public string Subfolder { get; init; }
     [JsonProperty("checkout")]
     public string Checkout { get; init; }
+    [JsonProperty("symlink")]
+    public bool Symlink { get; init; }
 
     [JsonConstructor]
     public AddonConfig(
       string url,
       string? subfolder,
-      string? checkout
+      string? checkout,
+      bool symlink = false
     ) {
       Url = url;
       Subfolder = subfolder ?? IApp.DEFAULT_SUBFOLDER;
       Checkout = checkout ?? IApp.DEFAULT_CHECKOUT;
+      Symlink = symlink;
     }
   }
 }
