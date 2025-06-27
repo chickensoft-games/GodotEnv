2f225378ca95f729ab29faece78e75353f772261
Mark Wilson <23439518+wlsnmrk@users.noreply.github.com>
2025-06-27 14:31:33 -0400

    test: add tests for config commands

---
+++ GodotEnv.Tests/src/features/config/commands/ConfigCommandTest.cs
@@ -0,0 +1,39 @@
+namespace Chickensoft.GodotEnv.Tests.Features.Config.Commands;
+
+using System.Threading.Tasks;
+using Chickensoft.GodotEnv.Common.Models;
+using Chickensoft.GodotEnv.Common.Utilities;
+using Chickensoft.GodotEnv.Features.Config.Commands;
+using CliFx.Infrastructure;
+using Moq;
+using Shouldly;
+using Xunit;
+
+public class ConfigCommandTest {
+
+  [Fact]
+  public async Task Executes() {
+    var systemInfo = new MockSystemInfo(OSType.Linux, CPUArch.X64);
+    var context = new Mock<IExecutionContext>();
+    var console = new FakeInMemoryConsole();
+    var log = new Log(systemInfo, console); // Use real log to test colors in output
+
+    context.Setup(ctx => ctx.SystemInfo).Returns(systemInfo);
+    context.Setup(ctx => ctx.CreateLog(console)).Returns(log);
+
+    var addonCommand = new ConfigCommand(context.Object);
+    await addonCommand.ExecuteAsync(console);
+
+    log.ToString().ShouldBe(
+      """
+
+      [style fg="yellow"]Please use a subcommand to manage configuration.
+
+      [/style][style]To see a list of available subcommands:
+
+      [/style][style fg="green"]    godotenv config --help
+
+      [/style]
+      """, StringCompareShould.IgnoreLineEndings);
+  }
+}
---
+++ GodotEnv.Tests/src/features/config/commands/set/ConfigSetCommandTest.cs
@@ -0,0 +1,92 @@
+namespace Chickensoft.GodotEnv.Tests.Features.Config.Commands.Set;
+
+using System.Threading.Tasks;
+using Chickensoft.GodotEnv.Common.Models;
+using Chickensoft.GodotEnv.Common.Utilities;
+using Chickensoft.GodotEnv.Features.Config.Commands.List;
+using CliFx.Infrastructure;
+using Moq;
+using Shouldly;
+using Xunit;
+
+public class ConfigSetCommandTest {
+  [Fact]
+  public async Task SetsIndicatedValue() {
+    var systemInfo = new MockSystemInfo(OSType.Linux, CPUArch.X64);
+    var context = new Mock<IExecutionContext>();
+    var console = new FakeInMemoryConsole();
+    var log = new Log(systemInfo, console); // Use real log to test colors in output
+
+    var config = new Config();
+    var testEmojiValue = !!config.ConfigValues.Terminal.DisplayEmoji;
+
+    context.Setup(ctx => ctx.SystemInfo).Returns(systemInfo);
+    context.Setup(ctx => ctx.CreateLog(console)).Returns(log);
+    context.Setup(ctx => ctx.Config).Returns(config);
+
+    var addonCommand = new ConfigSetCommand(context.Object) {
+      ConfigKey = "Terminal:DisplayEmoji",
+      ConfigValue = testEmojiValue.ToString(),
+    };
+    await addonCommand.ExecuteAsync(console);
+    config.ConfigValues.Terminal.DisplayEmoji.ShouldBe(testEmojiValue);
+  }
+
+  [Fact]
+  public async Task FailsGracefullyWhenIndicatedKeyDoesNotExist() {
+    var systemInfo = new MockSystemInfo(OSType.Linux, CPUArch.X64);
+    var context = new Mock<IExecutionContext>();
+    var console = new FakeInMemoryConsole();
+    var log = new Log(systemInfo, console); // Use real log to test colors in output
+
+    var config = new Config();
+
+    context.Setup(ctx => ctx.SystemInfo).Returns(systemInfo);
+    context.Setup(ctx => ctx.CreateLog(console)).Returns(log);
+    context.Setup(ctx => ctx.Config).Returns(config);
+
+    var addonCommand = new ConfigSetCommand(context.Object) {
+      ConfigKey = "FakeSection:FakeKey",
+      ConfigValue = "FakeValue"
+    };
+    await addonCommand.ExecuteAsync(console);
+
+    log.ToString().ShouldBe(
+      $"""
+
+      "{addonCommand.ConfigKey}" is not a valid configuration key, or "{addonCommand.ConfigValue}" is
+      not a valid value for "{addonCommand.ConfigKey}". Try "godotenv config list" for a
+      complete list of all entries.
+
+      """, StringCompareShould.IgnoreLineEndings);
+  }
+
+  [Fact]
+  public async Task FailsGracefullyWhenIndicatedValueIsInvalid() {
+    var systemInfo = new MockSystemInfo(OSType.Linux, CPUArch.X64);
+    var context = new Mock<IExecutionContext>();
+    var console = new FakeInMemoryConsole();
+    var log = new Log(systemInfo, console); // Use real log to test colors in output
+
+    var config = new Config();
+
+    context.Setup(ctx => ctx.SystemInfo).Returns(systemInfo);
+    context.Setup(ctx => ctx.CreateLog(console)).Returns(log);
+    context.Setup(ctx => ctx.Config).Returns(config);
+
+    var addonCommand = new ConfigSetCommand(context.Object) {
+      ConfigKey = "Terminal:DisplayEmoji",
+      ConfigValue = "FakeValue"
+    };
+    await addonCommand.ExecuteAsync(console);
+
+    log.ToString().ShouldBe(
+      $"""
+
+      "{addonCommand.ConfigKey}" is not a valid configuration key, or "{addonCommand.ConfigValue}" is
+      not a valid value for "{addonCommand.ConfigKey}". Try "godotenv config list" for a
+      complete list of all entries.
+
+      """, StringCompareShould.IgnoreLineEndings);
+  }
+}
