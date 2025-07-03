3045606ce5f59e992ed33917f3cf79bc981856d8
Mark Wilson <23439518+wlsnmrk@users.noreply.github.com>
2025-07-03 16:52:33 -0400

    test: for the pin command's underlying methods

--- GodotEnv.Tests/src/features/godot/domain/GodotVersionSpecifierRepositoryTest.cs
+++ GodotEnv.Tests/src/features/godot/domain/GodotVersionSpecifierRepositoryTest.cs
@@ -2,11 +2,15 @@ namespace Chickensoft.GodotEnv.Tests.Features.Config.Commands.List;
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Chickensoft.GodotEnv.Common.Clients;
 using Chickensoft.GodotEnv.Common.Utilities;
 using Chickensoft.GodotEnv.Features.Godot.Domain;
 using Chickensoft.GodotEnv.Features.Godot.Models;
+using Chickensoft.GodotEnv.Features.Godot.Serializers;
 using Moq;
 using Shouldly;
 using Xunit;
@@ -173,6 +177,175 @@ public class GodotVersionSpecifierRepositoryTest {
     var repo = new GodotVersionSpecifierRepository("test/working-dir", fileClient.Object);
 
     repo.InferVersion(Enumerate(fileMocks, m => m.Object), log.Object).ShouldBe(null);
+  }
+
+  [Fact]
+  public void ProjectDirectoryIsHighestSolutionIfGodotProjectLower() {
+    var directoryNames = new List<string> {
+      "/test/path/to/project",
+      "/test/path/to",
+      "/test/path",
+      "/test",
+      "/",
+    };
+    var directoryMocks = new List<Mock<IDirectoryInfo>> {
+      new(),
+      new(),
+      new(),
+      new(),
+      new(),
+    };
+    for (var i = 0; i < directoryMocks.Count; ++i) {
+      directoryMocks[i].Setup(dir => dir.FullName).Returns(directoryNames[i]);
+      if (i < directoryMocks.Count - 1) {
+        directoryMocks[i].Setup(dir => dir.Parent).Returns(directoryMocks[i + 1].Object);
+      }
+    }
+    var fileClient = new Mock<IFileClient>();
+    fileClient.Setup(
+        client => client.GetAncestorDirectories(directoryNames[0])
+      ).Returns(Enumerate(directoryMocks, m => m.Object));
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[0], "*.sln")
+    ).Returns((string dir, string pattern) => [$"{dir}/Other.sln"]);
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[2], "*.sln")
+    ).Returns((string dir, string pattern) => [$"{dir}/Project.sln"]);
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[1], "project.godot")
+    ).Returns((string dir, string pattern) => [$"{dir}/project.godot"]);
+
+    var repo = new GodotVersionSpecifierRepository(directoryNames[0], fileClient.Object);
+    repo.GetProjectDefinitionDirectory().ShouldBe(directoryNames[2]);
+  }
+
+  [Fact]
+  public void ProjectDirectoryIsHighestSolutionIfGodotProjectHigher() {
+    var directoryNames = new List<string> {
+      "/test/path/to/project",
+      "/test/path/to",
+      "/test/path",
+      "/test",
+      "/",
+    };
+    var directoryMocks = new List<Mock<IDirectoryInfo>> {
+      new(),
+      new(),
+      new(),
+      new(),
+      new(),
+    };
+    for (var i = 0; i < directoryMocks.Count; ++i) {
+      directoryMocks[i].Setup(dir => dir.FullName).Returns(directoryNames[i]);
+      if (i < directoryMocks.Count - 1) {
+        directoryMocks[i].Setup(dir => dir.Parent).Returns(directoryMocks[i + 1].Object);
+      }
+    }
+    var fileClient = new Mock<IFileClient>();
+    fileClient.Setup(
+        client => client.GetAncestorDirectories(directoryNames[0])
+      ).Returns(Enumerate(directoryMocks, m => m.Object));
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[0], "*.sln")
+    ).Returns((string dir, string pattern) => [$"{dir}/Other.sln"]);
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[2], "*.sln")
+    ).Returns((string dir, string pattern) => [$"{dir}/Project.sln"]);
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[3], "project.godot")
+    ).Returns((string dir, string pattern) => [$"{dir}/project.godot"]);
+
+    var repo = new GodotVersionSpecifierRepository(directoryNames[0], fileClient.Object);
+    repo.GetProjectDefinitionDirectory().ShouldBe(directoryNames[2]);
+  }
+
+  [Fact]
+  public void ProjectDirectoryIsHighestGodotProjectIfNoSln() {
+    var directoryNames = new List<string> {
+      "/test/path/to/project",
+      "/test/path/to",
+      "/test/path",
+      "/test",
+      "/",
+    };
+    var directoryMocks = new List<Mock<IDirectoryInfo>> {
+      new(),
+      new(),
+      new(),
+      new(),
+      new(),
+    };
+    for (var i = 0; i < directoryMocks.Count; ++i) {
+      directoryMocks[i].Setup(dir => dir.FullName).Returns(directoryNames[i]);
+      if (i < directoryMocks.Count - 1) {
+        directoryMocks[i].Setup(dir => dir.Parent).Returns(directoryMocks[i + 1].Object);
+      }
+    }
+    var fileClient = new Mock<IFileClient>();
+    fileClient.Setup(
+        client => client.GetAncestorDirectories(directoryNames[0])
+      ).Returns(Enumerate(directoryMocks, m => m.Object));
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[2], "project.godot")
+    ).Returns((string dir, string pattern) => [$"{dir}/project.godot"]);
+    fileClient.Setup(
+      client => client.GetFiles(directoryNames[3], "project.godot")
+    ).Returns((string dir, string pattern) => [$"{dir}/project.godot"]);
+
+    var repo = new GodotVersionSpecifierRepository(directoryNames[0], fileClient.Object);
+    repo.GetProjectDefinitionDirectory().ShouldBe(directoryNames[3]);
+  }
+
+  [Fact]
+  public void PinVersionWritesGlobalJsonForDotnetVersion() {
+    var projectDir = "/test/path";
+    var expectedRawJson =
+      /*lang=json,strict*/
+      """
+      {
+        "msbuild-sdks": {
+          "Godot.NET.Sdk": "4.4.1"
+        }
+      }
+      """;
+    var expectedNode = JsonNode.Parse(expectedRawJson);
+    var serializerOptions = new JsonSerializerOptions() { WriteIndented = true };
+
+    var path = "/test/path/global.json";
+    var version = new SpecificDotnetStatusGodotVersion(4, 4, 1, "stable", -1, true);
+    var writer = new Mock<TextWriter>();
+    var fileClient = new Mock<IFileClient>();
+    fileClient.Setup(client => client.Combine(projectDir, "global.json")).Returns(path);
+    fileClient.Setup(client => client.FileExists(path)).Returns(false);
+    fileClient.Setup(client => client.GetWriter(path)).Returns(writer.Object);
+    var log = new Mock<ILog>();
+    var repo = new GodotVersionSpecifierRepository(projectDir, fileClient.Object);
+
+    repo.PinVersion(version, projectDir, log.Object);
+
+    writer.Verify(wrt => wrt.WriteLine(expectedNode!.ToJsonString(serializerOptions)));
+    log.Verify(log => log.Info($"Writing Godot version \"{new IoVersionSerializer().SerializeWithDotnetStatus(version)}\" to {path}"));
+  }
+
+  [Fact]
+  public void PinVersionWritesGodotrcForNonDotnetVersion() {
+    var projectDir = "/test/path";
+    var path = "/test/path/.godotrc";
+    var version = new SpecificDotnetStatusGodotVersion(4, 4, 1, "stable", -1, false);
+    var serializer = new IoVersionSerializer();
+    var versionString = serializer.SerializeWithDotnetStatus(version);
+    var writer = new Mock<TextWriter>();
+    var fileClient = new Mock<IFileClient>();
+    fileClient.Setup(client => client.Combine(projectDir, ".godotrc")).Returns(path);
+    fileClient.Setup(client => client.FileExists(path)).Returns(false);
+    fileClient.Setup(client => client.GetWriter(path)).Returns(writer.Object);
+    var log = new Mock<ILog>();
+    var repo = new GodotVersionSpecifierRepository(projectDir, fileClient.Object);
+
+    repo.PinVersion(version, projectDir, log.Object);
+
+    writer.Verify(wrt => wrt.WriteLine(versionString));
+    log.Verify(log => log.Info($"Writing Godot version \"{versionString}\" to {path}"));
   }
 
   public IEnumerable<TResult> Enumerate<TResult, TCollection>(
